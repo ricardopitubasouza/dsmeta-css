8719ebb2904bce68d04c85ea031a5a89ebd45682
ricardopitubasouza <45132013+ricardopitubasouza@users.noreply.github.com>
2022-07-14 20:49:08 -0300

    Security Config

--- backend/src/main/java/com/devsuperior/vsmeta/VsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/vsmeta/VsmetaApplication.java
@@ -1,13 +1,13 @@
-package com.devsuperior.dsmeta;
+package com.devsuperior.vsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsmetaApplication {
+public class VsmetaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DsmetaApplication.class, args);
+		SpringApplication.run(VsmetaApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/devsuperior/vsmeta/VsmetaApplicationTests.java
+++ backend/src/test/java/com/devsuperior/vsmeta/VsmetaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devsuperior.dsmeta;
+package com.devsuperior.vsmeta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DsmetaApplicationTests {
+class VsmetaApplicationTests {
 
 	@Test
 	void contextLoads() {
