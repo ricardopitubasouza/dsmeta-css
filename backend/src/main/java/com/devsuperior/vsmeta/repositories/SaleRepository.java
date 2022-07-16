fe841d5e4534368ce755a90b3d8c115ca040677b
ricardopitubasouza <45132013+ricardopitubasouza@users.noreply.github.com>
2022-07-16 12:51:19 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/vsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.vsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.vsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
